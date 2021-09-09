c987932c15f84480bc50e0646185b3afcb8ed60a
15811480902 <lizong20001204@gmail.com>
2021-09-09 13:07:28 +0800

    移除多余引用

--- server/src/main/java/com/lz/server/ProviderRegister.java
+++ server/src/main/java/com/lz/server/ProviderRegister.java
@@ -2,8 +2,10 @@ package com.lz.server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
